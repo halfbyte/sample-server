e407e76a8f4074da78fd485e877489d9bd380877
Jan Krutisch <jan@krutisch.de>
2013-09-14 14:25:58 +0200

    inital checkin

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require "sinatra/json"
+require "sinatra/reloader" if development?
+
+require 'json'
+dir = File.absolute_path(ARGV[0] || 'testfiles')
+files = Dir[dir + "/*"]
+puts "Starting serving files from: #{dir}"
+puts files.map{|file| " - " + File.basename(file)}.join("\n")
+
+
+get '/index.json' do
+  mapping = files.inject({}) do |memo, file|
+    name = File.basename(file, File.extname(file))
+    url = to("/files/" + name + File.extname(file))
+    memo[name] = url
+    memo
+  end
+  headers "Access-Control-Allow-Origin" => "*"
+  
+  json mapping
+end
+
+get '/files/:file' do |file|
+  headers "Access-Control-Allow-Origin" => "*"
+  send_file File.join(dir, file)
+end
